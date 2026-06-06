917c75a8ab3e2bd13edba81542576e602917d14f
Vincent MONTÉCOT <11329528+vmonteco@users.noreply.github.com>
2026-06-06 06:40:57 +0200

    Enabled home-manager to manage itself

--- homes/vmonteco/default.nix
+++ homes/vmonteco/default.nix
@@ -9,6 +9,7 @@
   config = {
     home-manager.users.vmonteco = { pkgs, ... }: {
       programs.zsh.enable = true;
+      programs.home-manager.enable = true;
       home.stateVersion = "26.05";
     };
   };
