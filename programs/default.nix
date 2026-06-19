45055650945cb36e0b5ebfec4daed0d7b59f468b
Vincent MONTÉCOT <11329528+vmonteco@users.noreply.github.com>
2026-06-19 06:11:30 +0200

    Enabled Steam

--- programs/default.nix
+++ programs/default.nix
@@ -7,6 +7,16 @@
       # firefox.enable = true;
       zsh.enable = true;
       git.enable = true;
+      steam = {
+        enable = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+      };
     };
+
+    environment.systemPackages = with pkgs; [
+      protonup-qt
+    ];
   };
 }
